891b8d509ecc682e9554f38ad133c02e6225ac17
lindsey-k <doyoung.kwon7251@gmail.com>
2025-10-27 21:45:37 -0600

    Add files via upload

---
+++ Numpy.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+1. Import numpy as np and print the version number. (5 Points)
+
+ 
+
+ 
+
+2. Create a 1D array of numbers from 0 to 9. Desired output:
+
+#> array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+(10 Points)
+
+ 
+
+ 
+
+3. Import a dataset with numbers and texts keeping the text intact in python numpy. Use the iris dataset available from https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.dataLinks to an external site.. (20 Points)
+
+ 
+
+ 
+
+4. Find the position of the first occurrence of a value greater than 1.0 in petalwidth 4th column of iris dataset. Use the iris dataset available from https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.dataLinks to an external site.. (20 Points)
+
+ 
+
+ 
+
+5. From the array a, replace all values greater than 30 to 30 and less than 10 to 10.
+
+Input:
+
+np.random.seed(100)
+a = np.random.uniform(1,50, 20)
+
